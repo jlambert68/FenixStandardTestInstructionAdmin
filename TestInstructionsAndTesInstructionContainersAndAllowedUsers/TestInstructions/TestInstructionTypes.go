a249ea1fb7f5229aa46d80a00ae3a7a1092d3c58
jlambert68 <jonas.lambert@drakon.se>
2024-09-19 10:14:45 +0200

    Added type TestInstructionTypeXxxx_FenixSentToUsersDomain_FenixSendTemplateAddOn for Uuid and Name

--- TestInstructionsAndTesInstructionContainersAndAllowedUsers/TestInstructions/TestInstructionTypes.go
+++ TestInstructionsAndTesInstructionContainersAndAllowedUsers/TestInstructions/TestInstructionTypes.go
@@ -12,4 +12,7 @@ const (
 
 	TestInstructionTypeUUID_FenixSentToUsersDomain_SendToUserSpecifiedExecutionDomain TypeAndStructs.TestInstructionTypeUUIDType = "a61351f6-c0a3-4687-b28c-b5442f580c9d"
 	TestInstructionTypeName_FenixSentToUsersDomain_SendToUserSpecifiedExecutionDomain TypeAndStructs.TestInstructionTypeNameType = "SendToUserSpecifiedExecutionDomain"
+
+	TestInstructionTypeUUID_FenixSentToUsersDomain_FenixSendTemplateAddOn TypeAndStructs.TestInstructionTypeUUIDType = "fd94da57-3c9e-4a06-a824-c56828c67a00"
+	TestInstructionTypeName_FenixSentToUsersDomain_FenixSendTemplateAddOn TypeAndStructs.TestInstructionTypeNameType = "FenixSendTemplateAddOn"
 )
